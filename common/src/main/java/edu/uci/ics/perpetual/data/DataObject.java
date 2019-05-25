c37032b9abe043afcceb4f3ed2b78aa23f835944
mikhail <mikhail.lychagin@gmail.com>
2019-05-24 17:52:43 -0700

    [Common][Enrichment] DataObject implementation and IngestionThread API

--- common/src/main/java/edu/uci/ics/perpetual/data/DataObject.java
+++ common/src/main/java/edu/uci/ics/perpetual/data/DataObject.java
@@ -1,14 +1,25 @@
 package edu.uci.ics.perpetual.data;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import edu.uci.ics.perpetual.types.DataObjectType;
 import edu.uci.ics.perpetual.types.IType;
 
 public class DataObject {
+    private static JsonParser parser = new JsonParser();
+    private JsonObject object;
+    private DataObjectType type;
 
-
-    public DataObjectType getType() {
-        return null;
+    public DataObject(String data, DataObjectType type){
+        this.object = parser.parse(data).getAsJsonObject();
+        this.type = type;
     }
 
+    public JsonObject getObject() {
+        return object;
+    }
 
+    public DataObjectType getType() {
+        return type;
+    }
 }
