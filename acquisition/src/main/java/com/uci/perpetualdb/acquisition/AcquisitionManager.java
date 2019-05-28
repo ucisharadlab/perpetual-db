fa8e2c008f0292750fabbf8e7747cc2d1654c13e
shamalip <shamali.patwa@gmail.com>
2019-05-27 20:00:02 -0700

    Update getData Method with requestId param

--- acquisition/src/main/java/com/uci/perpetualdb/acquisition/AcquisitionManager.java
+++ acquisition/src/main/java/com/uci/perpetualdb/acquisition/AcquisitionManager.java
@@ -9,13 +9,13 @@ public class AcquisitionManager {
 
     AcquisitionManager instance;
     KafkaConsumer<Object, Object> consumer;
-    Properties kafkaconfigs;
+    private Properties kafkaConfigs;
     private AcquisitionManager(){
-         kafkaconfigs = new Properties();
-        // 환경 변수 설정
-        kafkaconfigs.put("bootstrap.servers", kafkaconfigs.get("bootstrap.servers"));     // kafka server host 및 port
-        kafkaconfigs.put("key.deserializer", kafkaconfigs.get("key.deserializer"));    // key deserializer
-        kafkaconfigs.put("value.deserializer", kafkaconfigs.get("value.deerializer"));  // value deserializer
+         kafkaConfigs = new Properties();
+        // Setting environment variables.
+        kafkaConfigs.put("bootstrap.servers", kafkaConfigs.get("bootstrap.servers"));     // kafka server host and port
+        kafkaConfigs.put("key.deserializer", kafkaConfigs.get("key.deserializer"));    // key deserializer
+        kafkaConfigs.put("value.deserializer", kafkaConfigs.get("value.deerializer"));  // value deserializer
     }
     public AcquisitionManager getInstance(){
         if(null != instance){
@@ -28,11 +28,10 @@ public class AcquisitionManager {
     /**
      * Invoked by Ingestion Engine
      */
-    public Object getData(){
+    public Object getData(String requestId){
         // TODO implement function - get complete data structure updated by various consumers.
-
-        consumer = new KafkaConsumer<Object, Object>(kafkaconfigs);    // consumer 생성
-        consumer.subscribe(Arrays.asList("TOPIC NAME"));      // topic 설정
+        consumer = new KafkaConsumer<Object, Object>( kafkaConfigs);    // consumer
+        consumer.subscribe(Arrays.asList(requestId));      // topic
 
         return null;
     }
