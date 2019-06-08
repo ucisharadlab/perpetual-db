d0c9d9fbc019d04dafa75114761940b8e4a4b771
omidkv <omidkv@gmail.com>
2019-06-07 22:23:35 -0700

    Start to jar injection

---
+++ enrichment/enrichTest/src/example/Enrichment.java
@@ -0,0 +1,15 @@
+package example;
+
+import com.google.gson.JsonObject;
+import edu.uci.ics.perpetual.data.DataObject;
+public class Enrichment {
+
+
+
+    public DataObject enrich(DataObject dataObject)
+    {
+
+        dataObject.getObject().addProperty("Test","Success");
+        return dataObject;
+    }
+}
--- enrichment/src/main/java/edu/uci/ics/perpetual/enrichment/EnrichmentFunction.java
+++ enrichment/src/main/java/edu/uci/ics/perpetual/enrichment/EnrichmentFunction.java
@@ -1,14 +1,46 @@
 package edu.uci.ics.perpetual.enrichment;
 
-public class EnrichmentFunction {
-    public final String functionName = null;
+import edu.uci.ics.perpetual.data.DataObject;
 
-    private EnrichmentFunction(){
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class EnrichmentFunction {
+
+    public final String functionName = null;
+    Class classToLoad;
+    Method method;
+    Object enrichmentInstance;
+    private EnrichmentFunction(String pathToJar){
+
+
+        try {
+            URLClassLoader child = new URLClassLoader (new URL[] {new URL(pathToJar)});
+
+            classToLoad = Class.forName("Enrichment", true, child);
+            method = classToLoad.getDeclaredMethod("enrich", DataObject.class);
+            enrichmentInstance = classToLoad.getDeclaredConstructor().newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
     }
 
     public static EnrichmentFunction getEnrichmentFunction(String pathToJar){
+
+        return new EnrichmentFunction(pathToJar);
+    }
+
+
+    public DataObject execute(DataObject dataObject) {
+
+        try {
+            return (DataObject) method.invoke(enrichmentInstance, dataObject);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
     }
-     
 }
