e83a5af445eeb95092009e8fae7e0cce735629dc
shamalip <shamali.patwa@gmail.com>
2019-06-08 23:07:03 -0700

    - Update load config method - update producer and consumers.

--- acquisition/aqCore/src/main/java/edu/uci/ics/perpetual/acquisition/utils/ScheduledStopTask.java
+++ acquisition/aqCore/src/main/java/edu/uci/ics/perpetual/acquisition/utils/ScheduledStopTask.java
@@ -1,20 +1,26 @@
 package edu.uci.ics.perpetual.acquisition.utils;
 
+import edu.uci.ics.perpetual.acquisition.datatypes.AcquisitionRequest;
+import edu.uci.ics.perpetual.acquisition.datatypes.RequestStatus;
+
 import java.util.TimerTask;
 
 public class ScheduledStopTask extends TimerTask {
 
     String taskName;
+    AcquisitionRequest request;
+    TimerTask producerTask;
 
-    TimerTask task;
-
-    public ScheduledStopTask(TimerTask task, String name){
-        this.task = task; this.taskName = name;
+    public ScheduledStopTask(TimerTask task, AcquisitionRequest request, String name){
+        System.out.println("ACQUISITION ENGINE: stopping task for producer of request " + request.getRequestId() +" scheduled.");
+        this.producerTask = task; this.taskName = name;
+        this.request = request;
     }
 
     @Override
     public void run() {
+        this.request.setStatus( RequestStatus.DONE );
         System.out.println("Stopping the task!!!" );
-        task.cancel();
+        producerTask.cancel();
     }
 }
