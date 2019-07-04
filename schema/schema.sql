2a5017fd549c687b1aefa11b76a4e3c70a944dfc
Alsaudia <alsaudia@uci.edu>
2019-07-04 16:48:33 -0700

    Update schema.sql

--- schema/schema.sql
+++ schema/schema.sql
@@ -48,6 +48,7 @@ CREATE TABLE TaggingFunction (
 	paramList TEXT NOT NULL,
 	returnTag TEXT NOT NULL,
 	cost INT NOT NULL,
+	quality DOUBLE(20,20) NOT NULL,
     path VARCHAR(300),
 
 	PRIMARY KEY (functionName)
