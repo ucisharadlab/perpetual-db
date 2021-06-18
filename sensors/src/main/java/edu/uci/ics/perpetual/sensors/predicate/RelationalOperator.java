09aacfe5d69204b5d2b556455979c14056461283
Sriram Rao <sriramrao@dhcp-10-8-019-233.mobile.reshsg.uci.edu>
2021-06-18 02:43:50 -0700

    Reformatted relationalOperator class

--- sensors/src/main/java/edu/uci/ics/perpetual/sensors/predicate/RelationalOperator.java
+++ sensors/src/main/java/edu/uci/ics/perpetual/sensors/predicate/RelationalOperator.java
@@ -8,25 +8,22 @@ public class RelationalOperator {
     public static String OR = "OR";
     public static String NOT = "NOT";
 
-    public static String binaryCombine(String operator, List<Predicate> predicates) {
-        return Constants.OPEN_PARENTHESIS
-                + predicates.stream().map(Predicate::toSql)
-                        .collect(Collectors.joining(Constants.SPACE + operator + Constants.SPACE))
-                + Constants.CLOSE_PARENTHESIS;
-    }
-
-    public static String unaryCombine(String operator, List<Predicate> predicates) {
-        if (predicates.size() < 1)
-            return null; // exception?
-
-        return Constants.OPEN_PARENTHESIS
-                + predicates.get(0).toSql() // if list has more than one element, the rest are ignored
-                + Constants.CLOSE_PARENTHESIS;
-    }
-
     public static String operate(String operator, List<Predicate> predicates) {
         if (NOT.equals(operator))
             return unaryCombine(operator, predicates);
         return binaryCombine(operator, predicates);
+    }
+
+    private static String binaryCombine(String operator, List<Predicate> predicates) {
+        return Constants.OPEN_PARENTHESIS
+                + predicates.stream().map(Predicate::toSql).collect(Collectors.joining(Constants.SPACE + operator + Constants.SPACE))
+                + Constants.CLOSE_PARENTHESIS;
+    }
+
+    private static String unaryCombine(String operator, List<Predicate> predicates) {
+        if (predicates.size() < 1)
+            return null; // exception?
+        // if list has more than one element, the rest are ignored
+        return Constants.OPEN_PARENTHESIS + predicates.get(0).toSql() + Constants.CLOSE_PARENTHESIS;
     }
 }
